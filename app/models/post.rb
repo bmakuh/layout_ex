0de147e3b5b4147184b6c279779b3d2ed1d1e9f2
Ben Makuh <alphari@gmail.com>
2011-09-29 10:33:06 -0600

    working on paperclip

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,13 @@
 class Post < ActiveRecord::Base
   attr_accessible :title, :artist, :description, :date, :price
+  
+  has_attached_file :photo, 
+                    :styles => {
+                      :thumb => "200x200>",
+                      :medium => "600x600>",
+                      :tiny => "25x25>"
+                    },
+                    :url => "/assets/:class/:attachment/:id/:style.:extension",
+                    :path => "#{RAILS_ROOT}/public/assets/:class/:attachment/:id/:style.:extension",
+                    :default_url => "/images/default_household_:style.jpg"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110929161128) do
+ActiveRecord::Schema.define(:version => 20110929162507) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -19,6 +19,10 @@ ActiveRecord::Schema.define(:version => 20110929161128) do
     t.decimal  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
 end
