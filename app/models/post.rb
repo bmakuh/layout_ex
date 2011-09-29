d74cdc6a00faad0241a0215c5dea80b8e7dbfc18
Ben Makuh <alphari@gmail.com>
2011-09-29 10:40:51 -0600

    paperclip

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,12 +2,6 @@ class Post < ActiveRecord::Base
   attr_accessible :title, :artist, :description, :date, :price
   
   has_attached_file :photo, 
-                    :styles => {
-                      :thumb => "200x200>",
-                      :medium => "600x600>",
-                      :tiny => "25x25>"
-                    },
                     :url => "/assets/:class/:attachment/:id/:style.:extension",
-                    :path => "#{RAILS_ROOT}/public/assets/:class/:attachment/:id/:style.:extension",
-                    :default_url => "/images/default_household_:style.jpg"
+                    :path => "#{RAILS_ROOT}/public/assets/:class/:attachment/:id/:style.:extension"
 end
