3711219a2db094b53be489f3cda5de18ca960274
Ben Makuh <alphari@gmail.com>
2011-10-03 15:55:30 -0600

    html5 + css3 layout example

--- app/models/post.rb
+++
@@ -1,7 +0,0 @@
-class Post < ActiveRecord::Base
-  attr_accessible :title, :artist, :description, :date, :price
-  
-  has_attached_file :photo, 
-                    :url => "/assets/:class/:attachment/:id/:style.:extension",
-                    :path => "#{RAILS_ROOT}/public/assets/:class/:attachment/:id/:style.:extension"
-end
--- config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '5a15ec3b3033b34755bb0742a8fdf43e1a0b9fb0c348e1a0c3dc53ddbb0b3fda563c56d0d5b3e72a3544d618f408610cb0399c69b0cbda3167102d9aefcf3a06';
